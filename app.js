6fb0346f1c809d8b73b7e536965efedf7abb5773
Oliver Schmidt <schmidt_oli@yahoo.de>
2023-10-28 21:26:16 +0200

    added express server, postman collection for testing api calls and extended documentation in readme.md

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
 require('dotenv').config();
-console.log("COBOL DIR: " + process.env.GNUCOBOL_DIRECTORY);
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT;
+
+// Parse JSON requests
+app.use(bodyParser.json());
+
+//import tax calculator class
+const TaxCalculator = require('./tax_calculator/tax.js');
+const taxCalculator = new TaxCalculator();
+
+// Route for calculating income tax
+app.post('/calculateIncomeTax', (req, res) => {
+    // Read the request body
+    const { grossIncome, maritalStatus, year } = req.body;
+  
+    // Default response
+    const incomeTax = taxCalculator.calculateIncomeTax(grossIncome, maritalStatus, year);
+  
+    // You can implement your income tax calculation logic here
+    // For now, just return the default
+    res.json({ incomeTax });
+  });
+  
+  // Default route for unimplemented routes
+  app.use((req, res) => {
+    res.status(501).json({ error: 'Not Implemented' });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
